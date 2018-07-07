751c9bd7f0ad1168e318e789fc735b84687db023
Leandro Beretta <lea.beretta@gmail.com>
2018-07-07 20:27:00 -0300

    cambios esteticos

--- config/jenkins/slaves/skopeo/skopeo.sh
+++ config/jenkins/slaves/skopeo/skopeo.sh
@@ -4,10 +4,10 @@
 oc import-image jenkins-slave-base-rhel7 --confirm --from=registry.access.redhat.com/openshift3/jenkins-slave-base-rhel7 -n openshift
 
 # Creates an image stream to hold the skopeo image
-oc create is jenkins-slave-skopeo -n openshift
+oc create is skopeo -n openshift
 
 # Creates the build config
 oc create -f skopeo-bc.yaml -n openshift
 
 # Creates a new build
-oc start-build jenkins-slave-skopeo -n openshift
+oc start-build skopeo -n openshift
