4969126efc9961e2f8067f1778974ac59458a9e6
Leandro Beretta <lea.beretta@gmail.com>
2018-09-23 00:02:52 -0300

    api commons created, minor changes

--- api/commons.groovy
+++ api/commons.groovy
@@ -13,18 +13,14 @@ def gitCheckout(repo, branch, secret) {
 }
 
 def buildImage(app, artifactsDir) {
-
     // If artifacts dir is set, binary s2i build is used
-    
     if (artifactsDir)
         openshift.selector("bc", app).startBuild("--from-dir=${artifactsDir}", "--wait=true")
     else 
-        openshift.selector("bc", app).startBuild("--wait=true")
-        
+        openshift.selector("bc", app).startBuild("--wait=true")       
 }
 
 def deployApplication(app, tag) {
-    openshift.verbose()
     if (!openshift.selector("dc", app).exists()) {
         // The creation starts a deployment
         createApplication(app, tag)                 
@@ -35,14 +31,14 @@ def deployApplication(app, tag) {
     //verifyDeployment(app)
 }
 
-def updateApplication(app, tag) {
+def updateApplication(app, image, tag) {
     openshift.set("triggers", "dc/${app}", "--remove-all")
-    openshift.set("triggers", "dc/${app}", "--from-image=${app}:${tag}", "-c ${app}")
+    openshift.set("triggers", "dc/${app}", "--from-image=${image}:${tag}", "-c ${app}")
 }
 
-def createApplication(app, tag) {
+def createApplication(app, image, tag) {
     // Creates the application and get the brand new BuildConfig
-    def dc = openshift.newApp("${app}:${tag}").narrow("dc");
+    def dc = openshift.newApp("${image}:${tag}", "--name=${app}").narrow("dc");
     // Creates the app Route
     openshift.selector("svc", app).expose();                
 }
