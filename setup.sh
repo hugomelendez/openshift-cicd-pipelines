37c95f6ce7e20536eb195f3147cdae981471baad
Leandro Beretta <lea.beretta@gmail.com>
2018-09-23 22:55:15 -0300

    algunos cambios

--- setup.sh
+++ setup.sh
@@ -9,29 +9,62 @@ minishift start
 # Logs in as admin to create projects
 oc login "https://$(minishift ip):8443" -u admin -p admin
 
-# Creates projects for development (n teams, n projects)
-oc new-project dev1
-oc new-project dev2
-# Creates the test project
-oc new-project test
-# Creates the prod (management) project
-oc new-project prod
+# Creates projects for development (n areas, n projects)
+oc new-project core-dev
+oc new-project apis-dev
 
-# Creates the development templates
-oc create -f ./environments/dev/dev-pipelines-template.yaml -n dev1
-oc create -f ./environments/dev/branch-pipeline-template.yaml -n dev1
-oc create -f ./environments/dev/dev-pipelines-template.yaml -n dev2
-oc create -f ./environments/dev/branch-pipeline-template.yaml -n dev2
+# Creates the test projects (n areas, n projects)
+oc new-project core-test
+oc new-project apis-test
 
-# Creates the test template
-oc create -f ./environments/test/external/test-external-image-pipeline-template.yaml -n test
-oc create -f ./environments/test/internal/test-pipeline-template.yaml -n test
+# Creates the prod (management) projects (n areas, n projects)
+oc new-project core-prod-management
+oc new-project apis-prod-management
 
-# Creates the prod template
-oc create -f ./environments/prod/prod-pipeline-template.yaml -n prod
+# Creates the development templates in the development projects
+oc create -f ./environments/dev/dev-pipelines-template.yaml -n core-dev
+oc create -f ./environments/dev/branch-pipeline-template.yaml -n core-dev
+oc create -f ./environments/dev/dev-pipelines-template.yaml -n apis-dev
+oc create -f ./environments/dev/branch-pipeline-template.yaml -n apis-dev
+
+# Creates the test template in the test projects
+oc create -f ./environments/test/test-pipeline-template.yaml -n core-test
+oc create -f ./environments/test/test-pipeline-template.yaml -n apis-test
+
+# Creates the prod template in the prod projects
+oc create -f ./environments/prod/prod-pipeline-template.yaml -n core-prod-management
+oc create -f ./environments/prod/prod-pipeline-template.yaml -n apis-prod-management
+
+# Gogs
+
+oc new-project gogs
+
+oc new-app -f http://bit.ly/openshift-gogs-persistent-template --param=HOSTNAME="gogs-gogs.$(minishift ip).nip.io" -n gogs
+
+# Jenkins
+
+# Creates the Jenkins project
+oc new-project jenkins
+
+# Deploys Jenkins
+oc new-app jenkins-persistent -n jenkins
+
+# Sets Jenkins service account permissions
+oc adm policy add-role-to-user edit system:serviceaccount:jenkins:jenkins -n core-dev
+oc adm policy add-role-to-user edit system:serviceaccount:jenkins:jenkins -n apis-dev
+oc adm policy add-role-to-user edit system:serviceaccount:jenkins:jenkins -n core-test
+oc adm policy add-role-to-user edit system:serviceaccount:jenkins:jenkins -n apis-test
+oc adm policy add-role-to-user edit system:serviceaccount:jenkins:jenkins -n core-prod-management
+oc adm policy add-role-to-user edit system:serviceaccount:jenkins:jenkins -n apis-prod-management
 
 # Creates a new cluster role for reading groups in the cluster
 oc create -f ./configuration/roles/group-reader.yaml
+
+# Allows jenkins service account to read groups from cluster
+oc adm policy add-cluster-role-to-user group-reader system:serviceaccount:jenkins:jenkins
+
+# Avoids a Jenkins instance in every project a pipeline is created
+minishift openshift config set --patch '{"jenkinsPipelineConfig":{"autoProvisionEnabled":false}}'
 
 # Creates new groups
 oc adm groups new developers
@@ -41,41 +74,39 @@ oc adm groups new test-approvers
 oc adm groups new prod-approvers
 
 # Adds users to groups
-oc adm groups add-users developers developer1 developer2 developer3 developer4
-oc adm groups add-users testers tester1 tester2
-oc adm groups add-users administrators administrator1
-oc adm groups add-users test-approvers test-approver1 test-approver2
-oc adm groups add-users prod-approvers prod-approver1
+oc adm groups add-users developers leandro carlos hugo ana
+oc adm groups add-users testers maria diego
+oc adm groups add-users administrators mario
+oc adm groups add-users test-approvers jose hernan
+oc adm groups add-users prod-approvers hernan
 
 # Sets permissions
 
-# Allows jenkins service account from test to edit development projects
-oc adm policy add-role-to-user edit system:serviceaccount:test:jenkins -n dev1
-oc adm policy add-role-to-user edit system:serviceaccount:test:jenkins -n dev2
-
-# Allows jenkins service account to read groups from cluster
-oc adm policy add-cluster-role-to-user group-reader system:serviceaccount:test:jenkins
-oc adm policy add-cluster-role-to-user group-reader system:serviceaccount:prod:jenkins
-
 # Project memberships
-oc adm policy add-role-to-user admin developer1 -n dev1
-oc adm policy add-role-to-user admin developer2 -n dev1
+oc adm policy add-role-to-user admin leandro -n core-dev
+oc adm policy add-role-to-user admin hugo -n core-dev
+oc adm policy add-role-to-user admin leandro -n core-test
+oc adm policy add-role-to-user admin hugo -n core-test
 
-oc adm policy add-role-to-user admin developer1 -n dev2
-oc adm policy add-role-to-user admin developer2 -n dev2
+oc adm policy add-role-to-user admin carlos -n apis-dev
+oc adm policy add-role-to-user admin ana -n apis-dev
+oc adm policy add-role-to-user view carlos -n apis-test
+oc adm policy add-role-to-user view ana -n apis-test
 
-oc adm policy add-role-to-group admin administrators -n dev1
-oc adm policy add-role-to-group admin administrators -n dev2
+oc adm policy add-role-to-group admin administrators -n core-dev
+oc adm policy add-role-to-group admin administrators -n apis-dev
+oc adm policy add-role-to-group admin administrators -n core-test
+oc adm policy add-role-to-group admin administrators -n apis-test
+oc adm policy add-role-to-group admin administrators -n core-prod-management
+oc adm policy add-role-to-group admin administrators -n apis-prod-management
+oc adm policy add-role-to-group admin administrators -n jenkins
+oc adm policy add-role-to-group admin administrators -n gogs
 
-oc adm policy add-role-to-group admin administrators -n test
-oc adm policy add-role-to-group admin administrators -n prod
-oc adm policy add-role-to-group edit test-approvers -n test
-oc adm policy add-role-to-group edit prod-approvers -n prod
-oc adm policy add-role-to-group view developers -n test
+oc adm policy add-role-to-group edit test-approvers -n core-test
+oc adm policy add-role-to-group edit test-approvers -n apis-test
 
-oc adm policy add-role-to-user view system:serviceaccount:test:jenkins -n dev1
-oc adm policy add-role-to-user view system:serviceaccount:test:jenkins -n dev2
-oc adm policy add-role-to-user view system:serviceaccount:prod:jenkins -n test
+oc adm policy add-role-to-group edit prod-approvers -n core-prod-management
+oc adm policy add-role-to-group edit prod-approvers -n apis-prod-management
 
 # Exposes the non-prod cluster registry
 minishift addons apply registry-route
@@ -94,17 +125,11 @@ oc create -f ./configuration/jenkins/slaves/skopeo/skopeo-bc.yaml -n openshift
 # Creates a new build
 oc start-build skopeo -n openshift --wait
 
-# Tags the skopeo image in the test project 
-oc tag openshift/skopeo:latest skopeo:latest -n test
+# Tags the skopeo image in the jenkins project 
+oc tag openshift/skopeo:latest skopeo:latest -n jenkins
 
-# Adds a label for the sync plugin to push the image into the test jenkins instance
-oc label is skopeo role=jenkins-slave -n test
-
-# Tags the skopeo image in the prod project 
-oc tag openshift/skopeo:latest skopeo:latest -n prod
-
-# Adds a label for the sync plugin to push the image into the prod jenkins instance
-oc label is skopeo role=jenkins-slave -n prod
+# Adds a label for the sync plugin to push the image into the jenkins instance
+oc label is skopeo role=jenkins-slave -n jenkins
 
 # Sets the MiniShift profile for the prod cluster
 minishift profile set prod
@@ -115,12 +140,16 @@ minishift start
 # Logs in as admin to create projects
 oc login https://$(minishift ip):8443 -u admin -p admin
 
-# Creates the prod project 
-oc new-project prod
+# Creates the prod projects
+oc new-project core-prod
+oc new-project apis-prod
 
 # Creates an admin service account for deployments, etc
 oc create sa admin -n prod
-oc adm policy add-role-to-user admin system:serviceaccount:prod:admin -n prod
+
+# Sets permissions
+oc adm policy add-role-to-user admin system:serviceaccount:prod:admin -n core-prod
+oc adm policy add-role-to-user admin system:serviceaccount:prod:admin -n apis-prod
 
 # Exposes the prod cluster registry
 minishift addons apply registry-route
