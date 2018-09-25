f8686aef097c9c427d1b4e5f57cf879f5ca6d880
Leandro Beretta <lea.beretta@gmail.com>
2018-09-25 12:16:03 -0300

    se desarrolla jenkins shared library

--- vars/getGitConfig.groovy
+++ vars/getGitConfig.groovy
@@ -5,6 +5,7 @@ def call(repo, branch, secret) {
 
     gitInfo['url'] = repo
     gitInfo['branch'] = branch
+    echo env.GIT_SECRET
     if (env.GIT_SECRET && !secret.equals("none"))
         gitInfo['credentialsId'] = secret
 
