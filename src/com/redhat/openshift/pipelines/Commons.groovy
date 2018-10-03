cab70529339f7aff00ad0eae6a37d86511f9235c
Leandro Beretta <lea.beretta@gmail.com>
2018-10-03 12:11:25 -0300

    nodejs getversion method added

--- src/com/redhat/openshift/pipelines/Commons.groovy
+++ src/com/redhat/openshift/pipelines/Commons.groovy
@@ -38,9 +38,9 @@ class Commons implements Serializable {
     }
 
     def getVersionFromPackageJSON() {
-        sh "cat package.json | grep version | head -1 | awk -F: '{ print \$2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]' > version"
+        steps.sh "cat package.json | grep version | head -1 | awk -F: '{ print \$2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]' > version"
 
-        return readFile("version").trim()
+        return steps.readFile("version").trim()
     }
 
     def getApplicationProject() {
