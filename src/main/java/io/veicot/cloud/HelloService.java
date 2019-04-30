683cce898cc2e676a88f653e31ba7c60ccd517c2
Leandro Beretta <lea.beretta@gmail.com>
2019-04-30 15:25:10 -0300

    migrating to quarkus

--- src/main/java/io/veicot/cloud/HelloService.java
+++ src/main/java/io/veicot/cloud/HelloService.java
@@ -1,31 +1,30 @@
 package io.veicot.cloud;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
-
 @Path("/")
-public class HelloService extends Application {
+public class HelloService {
 
 	@GET
 	@Produces("text/plain")
-	public Response doGet() {
+	public String doGet() {
 		Properties properties = this.loadProperties("artifact.properties");
 
-		return Response.ok(String.format("%s (Version: %s - Build Number: %s - Git Commit: %s - Environment: %s)", 
-										 System.getenv("HELLO_STRING"), 
-										 properties.get("artifact.version"),
-										 properties.get("artifact.buildNumber"),
-										 properties.get("artifact.gitCommit"),
-										 System.getenv("HELLO_ENVIRONMENT"))).build();
-	}
+		return String.format("%s (Version: %s - Build Number: %s - Git Commit: %s - Environment: %s)", 
+							 System.getenv("HELLO_STRING"), 
+							 properties.get("artifact.version"),
+							 properties.get("artifact.buildNumber"),
+							 properties.get("artifact.gitCommit"),
+							 System.getenv("HELLO_ENVIRONMENT"));
+}
 
 	private Properties loadProperties(String fileName) {
         InputStream is = getClass().getClassLoader().getResourceAsStream(fileName);
